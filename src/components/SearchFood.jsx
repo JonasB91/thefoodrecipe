10383ab41a24007a87471e11ce5cf5f14fd3f3f7
JonasB91 <jonas.backas@hotmail.com>
2024-04-04 23:56:05 +0200

    Validering i search field, och error hantering

--- src/components/SearchFood.jsx
+++ src/components/SearchFood.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import '../css/SearchFood.css';
 import FoodCard from './FoodCard';
 import FoodDetails from './FoodDetails';
@@ -10,17 +10,30 @@ function SearchFood() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [showFoodDetails, SetShowFoodDetails] = useState(false) // låter mig kolla om jag ska visa food details eller inte
+    const [searchError, setSearchError] = useState(null); // State för att hantera search field error messege,
+    const searchRef = useRef(null); // Sätter State för useRef
 
     // eventhandler för att söka
     const handleSearchChange = (event) => {
         setSearchFood(event.target.value);
+        //Rensa tidigare search error messege när en user börjar skriva igen.
+        setSearchError(null);
     };
+    
     
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        //Validation for search input,
+        if(searchRef.current.value.trim() === '') {
+            setSearchError('ERROR; Please enter a food dish!'); // sätter error messege här
+            searchRef.current.focus()  // fokus på inputfield
+                return;
+        }
+
         setLoading(true);
         try {
-            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchFood}`);
+            const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchFood}`);
             const data = await response.json();
             setSearchResults(data.meals);
             console.log(data);
@@ -47,10 +60,20 @@ function SearchFood() {
         <div className="banner-container">
         <div className="search-container">
             <form onSubmit={handleSubmit}>
-                <input className="input-field" type="text" value={searchFood} onChange={handleSearchChange} />
+                <input
+                 ref={searchRef}
+                 className="input-field"
+                 type="text" value={searchFood} 
+                 onChange={handleSearchChange}
+                />
                 <button className="search-btn" type="submit">Search</button>
             </form>
         </div>
+        {searchError && (
+            <div className='error-message-container'>
+                <p className='error-message'>{searchError}</p>
+                </div>
+            )}
         {loading && <p>Loading...</p>}
         {error && <p>Error: {error.message}</p>}
         {showFoodDetails ? (
